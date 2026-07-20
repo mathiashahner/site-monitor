2fa81a69c4eafefd3f9517f7f5a579683df3a6fe
Mathias Hahner <mathias_hahner@hotmail.com>
2026-07-19 22:57:06 -0300

    feat: handling errors

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import 'dotenv/config'
 import { chromium } from 'playwright'
 import { requests } from './requests.js'
 
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL
+const IS_DEV_ENV = process.env.IS_DEV_ENV
 const BROWSERLESS_TOKEN = process.env.BROWSERLESS_TOKEN
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL
 
 const scraping = async () => {
-  const browser = 0
-    ? await chromium.connect({
+  const browser = IS_DEV_ENV
+    ? await chromium.launch({ headless: false })
+    : await chromium.connect({
         wsEndpoint: `wss://production-sfo.browserless.io/?token=${BROWSERLESS_TOKEN}`,
       })
-    : await chromium.launch({ headless: false })
 
   const page = await browser.newPage()
   let message = '🛎️ Prices update:\n'
@@ -18,13 +19,17 @@ const scraping = async () => {
   for (const request of requests) {
     try {
       await page.goto(request.url, { waitUntil: 'domcontentloaded' })
-      await page.waitForSelector(request.selector)
 
-      const value = await page.locator(request.selector).first().textContent()
+      const value = await Promise.all(
+        request.steps.map(async (step) => {
+          await page.waitForSelector(step.selector, { timeout: 5000 })
+          return await page.locator(step.selector).first().textContent()
+        }),
+      )
 
       message += `\n${request.message}: ${value}`
     } catch (error) {
-      message += `\n${request.message}: Error getting price`
+      message += `\n${request.message}: Not found`
       console.error(`Error processing ${request.name}:`, error.message)
     }
   }
--- requests.js
+++ requests.js
@@ -1,9 +1,28 @@
 export const requests = [
   {
-    name: 'msc',
-    message: '🚢 MSC Cruise',
-    selector: '.prices__main-price span',
+    name: 'msc-virtuosa',
+    message: '🚢 MSC Virtuosa',
+    url: 'https://www.msccruzeiros.com.br/Search%20Result?area=SOA&embkPort=SSZ&departureDateFrom=01%2F11%2F2026&departureDateTo=30%2F04%2F2027&passengers=2%7C0%7C0%7C0&page=1&ships=VI&nights=6%2C7',
+    steps: [{ selector: '.prices__main-price span' }],
+  },
+  {
+    name: 'msc-virtuosa-05-12',
+    message: '🚢 MSC Virtuosa 05/12',
     url: 'https://www.msccruzeiros.com.br/Search%20Result?area=SOA&embkPort=SSZ&departureDateFrom=05%2F12%2F2026&departureDateTo=05%2F12%2F2026&passengers=2%7C0%7C0%7C0&page=1&ships=VI&nights=6%2C7',
+    steps: [{ selector: '.prices__main-price span' }],
+  },
+  {
+    name: 'msc-voyagers-club',
+    message: '🚢 MSC Voyagers Club',
+    url: 'https://www.msccruzeiros.com.br/gerenciar-reserva/msc-voyagers-club/msc-voyagers-selection-15?departureDateFrom=05%2F12%2F2026&departureDateTo=05%2F12%2F2026&passengers=2%7C0%7C0%7C0&area=SOA&embkPort=SSZ&nights=6%2C7',
+    steps: [{ selector: '.prices__main-price span' }],
+  },
+  {
+    name: 'msc-super-bingo',
+    message: '🚢 MSC Super Bingo',
+    // url: 'https://www.msccruzeiros.com.br/ofertas/super-bingo?departureDateFrom=21%2F07%2F2026&departureDateTo=21%2F06%2F2029&passengers=2%7C0%7C0%7C0&area=SOA&nights=6%2C7',
+    url: 'https://www.msccruzeiros.com.br/ofertas/super-bingo?departureDateFrom=21%2F07%2F2026&departureDateTo=21%2F06%2F2029&passengers=2%7C0%7C0%7C0&area=SOA&nights=6%2C7&ships=VI',
+    steps: [{ selector: '.prices__main-price span' }],
   },
   // {
   //   name: 'skyscanner',
